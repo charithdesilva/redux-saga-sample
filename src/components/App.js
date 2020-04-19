027592f1918e50ca18c448f951e3ddf5723d5a96
Charith De Silva <bitbucket-admin@insighture.com>
2020-04-19 15:06:46 +0530

    continues calls to color api

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Redux Saga</h1>
+        <h1>Redux Saga - COLR API</h1>
         <div>
           {this.props.colorAPI.colors.map((color, i) => (
             <div key={i}>
